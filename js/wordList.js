f9627eff845f8eaddca49fb8588bdd0e96999a0a
Hazem yakout <143601439+hazem-yakout@users.noreply.github.com>
2024-07-08 08:09:21 -0700

    Add files via upload

---
+++ js/wordList.js
@@ -0,0 +1,192 @@
+const wordList = [
+  {
+    word: "python",
+    hint: "programming language",
+  },
+  {
+    word: "guitar",
+    hint: "a musical instrument",
+  },
+  {
+    word: "aim",
+    hint: "a purpose or intention",
+  },
+  {
+    word: "mars",
+    hint: "planet of our solar system",
+  },
+  {
+    word: "gold",
+    hint: "a yellow precious metal",
+  },
+  {
+    word: "shein",
+    hint: "online shopping site",
+  },
+  {
+    word: "javascript",
+    hint: "programming language",
+  },
+  {
+    word: "coding",
+    hint: "related to programming",
+  },
+  {
+    word: "titanic",
+    hint: "action and amazing movie",
+  },
+  {
+    word: "jeans",
+    hint: "related to clothes",
+  },
+  {
+    word: "avatar",
+    hint: "epic science fiction film",
+  },
+  {
+    word: "gif",
+    hint: "a file format for image",
+  },
+  {
+    word: "mental",
+    hint: "related to the mind",
+  },
+  {
+    word: "map",
+    hint: "diagram represent of an area",
+  },
+  {
+    word: "island",
+    hint: "land surrounded by water",
+  },
+  {
+    word: "hockey",
+    hint: "a famous outdoor game",
+  },
+  {
+    word: "chess",
+    hint: "related to an indoor game",
+  },
+  {
+    word: "tiktok",
+    hint: "a social media app",
+  },
+  {
+    word: "github",
+    hint: "code hosting platform",
+  },
+  {
+    word: "pizza",
+    hint: "a good type of food",
+  },
+  {
+    word: "silver",
+    hint: "precious greyish-white metal",
+  },
+  {
+    word: "mobile",
+    hint: "an electronic device",
+  },
+  {
+    word: "computer",
+    hint: "a new device and technology",
+  },
+
+  {
+    word: "google",
+    hint: "famous search engine",
+  },
+
+  {
+    word: "excel",
+    hint: "microsoft product for windows",
+  },
+  {
+    word: "mysql",
+    hint: "a relational database system",
+  },
+  {
+    word: "ronaldo",
+    hint: "one of best players in the world",
+  },
+  {
+    word: "messi",
+    hint: "one of the best players in the history",
+  },
+  {
+    word: "euro",
+    hint: "a currency",
+  },
+  {
+    word: "tesla",
+    hint: "unit of magnetic flux density",
+  },
+  {
+    word: "mars",
+    hint: "planet of our solar system",
+  },
+  {
+    word: "proxy",
+    hint: "related to server application",
+  },
+  {
+    word: "email",
+    hint: "related to exchanging message",
+  },
+  {
+    word: "html",
+    hint: "markup language for the web",
+  },
+  {
+    word: "air",
+    hint: "related to a gas",
+  },
+  {
+    word: "idea",
+    hint: "a thought or suggestion",
+  },
+  {
+    word: "server",
+    hint: "related to computer or system",
+  },
+  {
+    word: "elsaka",
+    hint: "an egyptian actor",
+  },
+  {
+    word: "salah",
+    hint: "an important player in egypt",
+  },
+  {
+    word: "search",
+    hint: "act to find something",
+  },
+  {
+    word: "key",
+    hint: "small piece of metal",
+  },
+  {
+    word: "egypt",
+    hint: "a country name",
+  },
+  {
+    word: "joker",
+    hint: "psychological thriller film",
+  },
+  {
+    word: "dubai",
+    hint: "developed country name",
+  },
+  {
+    word: "photo",
+    hint: "representation of person or scene",
+  },
+  {
+    word: "nile",
+    hint: "largest river in the world",
+  },
+  {
+    word: "rain",
+    hint: "related to a water",
+  },
+];
